31f44ade3fb1fedfa0c8fa8fdde8b3b9cfbc1681
Lukecp <lukecp@pm.me>
2021-01-19 23:13:17 -0500

    completed mvp

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,9 @@
+import useLocalStorage from './useLocalStorage';
+
+const useDarkMode = (initialValue) => {
+	const [darkMode, setDarkMode] = useLocalStorage("dark", initialValue)
+
+	return [darkMode, setDarkMode];
+};
+
+export default useDarkMode;
